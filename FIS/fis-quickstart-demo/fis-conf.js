aef241f89937824ab252baa58a8889640f8d5077
lancao03 <958566776@qq.com>
2016-09-13 15:36:22 +0800

    解决冲突

--- FIS/fis-quickstart-demo/fis-conf.js
+++ FIS/fis-quickstart-demo/fis-conf.js
@@ -1,31 +1,11 @@
-//Step 1. 取消下面的注释开启simple插件，注意需要先进行插件安装 npm install -g fis-postpackager-simple
- fis.config.set('modules.postpackager', 'simple');
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+  spriter: fis.plugin('csssprites')
+})
 
-//通过pack设置干预自动合并结果，将公用资源合并成一个文件，更加利于页面间的共用
+// 对 CSS 进行图片合并
+fis.match('*.css', {
+  // 给匹配到的文件分配属性 `useSprite`
+  useSprite: true
+});
 
-//Step 2. 取消下面的注释开启pack人工干预
- fis.config.set('pack', {
-     'pkg/lib.js': [
-         '/lib/mod.js',
-         '/modules/underscore/**.js',
-         '/modules/backbone/**.js',
-         '/modules/jquery/**.js',
-         '/modules/vendor/**.js',
-         '/modules/common/**.js'
-     ],
-     'pkg/aio.css':[
-         '**.css'
-     ]
- });
-
-//Step 3. 取消下面的注释可以开启simple对零散资源的自动合并
-// fis.config.set('settings.postpackager.simple.autoCombine', true);
-fis.config.set('modules.parser.less','less');
-//以less结尾的替换成css结尾的
-fis.config.set('roadmap.ext.less','css');
-//Step 4. 取消下面的注释开启图片合并功能
-// fis.config.set('roadmap.path', [{
-//     reg: '**.css',
-//     useSprite: true
-// }]);
-// fis.config.set('settings.spriter.csssprites.margin', 20);
